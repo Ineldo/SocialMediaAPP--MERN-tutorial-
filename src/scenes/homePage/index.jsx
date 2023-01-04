8b1d7b5ea0d3657de5e99afb8df47a1477a41f74
@Ineldo_39 <ineldvascon@gmail.com>
2023-01-04 17:13:15 -0100

    almost done

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -3,6 +3,9 @@ import {useSelector} from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidgets from "scenes/widgets/MyPostWidgets";
+import Posts_Widgets from "scenes/widgets/Posts_Widgets";
+import FriendListWidgets from "scenes/widgets/FriendListWidgets";
+import AdvertWidget from "scenes/widgets/AdvertWidgets";
 
 const HomePage=()=>{
     const isNonMobileScreens =useMediaQuery("(min-width:1000px)");
@@ -26,12 +29,15 @@ const HomePage=()=>{
                     mt={isNonMobileScreens ? undefined : "2rem"}
                     >
                     <MyPostWidgets picturePath={picturePath}/>
+                    <Posts_Widgets userId={_id}/>
                 </Box>
                 {isNonMobileScreens && (
-                <Box flexBasis="26%">
-                
-                </Box>
-                )}
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidgets userId={_id} />
+          </Box>
+        )}
             </Box>
         </Box>
     ) 
